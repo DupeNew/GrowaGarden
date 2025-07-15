2e2e96a504832a4df77e7483481795d65be6906e
DupeNew <glemarcaomateplong@gmail.com>
2025-07-15 23:04:51 +0800

    Create PetEggs.lua

---
+++ PetEggs.lua
@@ -0,0 +1,71 @@
+--// Data Source: PetEggs.lua
+--// Contains the loot table information for each egg.
+
+return {
+	["Legendary Egg"] = {
+		RarityData = { Items = { ["Silver Monkey"] = { ItemOdd = 20 }, Cow = { ItemOdd = 20 }, ["Sea Otter"] = { ItemOdd = 5 }, Turtle = { ItemOdd = 1 }, ["Polar Bear"] = { ItemOdd = 1 } } }
+	},
+	["Rare Egg"] = {
+		RarityData = { Items = { ["Orange Tabby"] = { ItemOdd = 20 }, Monkey = { ItemOdd = 5 }, ["Spotted Deer"] = { ItemOdd = 15 }, Rooster = { ItemOdd = 10 }, Pig = { ItemOdd = 10 } } }
+	},
+	["Uncommon Egg"] = {
+		RarityData = { Items = { ["Black Bunny"] = { ItemOdd = 5 }, Cat = { ItemOdd = 5 }, Deer = { ItemOdd = 5 }, Chicken = { ItemOdd = 5 } } }
+	},
+	["Common Egg"] = {
+		RarityData = { Items = { Dog = { ItemOdd = 10 }, ["Golden Lab"] = { ItemOdd = 10 }, Bunny = { ItemOdd = 10 } } }
+	},
+	["Mythical Egg"] = {
+		RarityData = { Items = { ["Grey Mouse"] = { ItemOdd = 36 }, Squirrel = { ItemOdd = 27 }, ["Brown Mouse"] = { ItemOdd = 27 }, ["Red Giant Ant"] = { ItemOdd = 8.5 }, ["Red Fox"] = { ItemOdd = 1.5 } } }
+	},
+	["Bug Egg"] = {
+		RarityData = { Items = { Dragonfly = { ItemOdd = 1 }, ["Praying Mantis"] = { ItemOdd = 4 }, ["Giant Ant"] = { ItemOdd = 25 }, Snail = { ItemOdd = 30 }, Caterpillar = { ItemOdd = 40 } } }
+	},
+	["Exotic Bug Egg"] = {
+		RarityData = { Items = { Dragonfly = { ItemOdd = 1 }, ["Praying Mantis"] = { ItemOdd = 4 }, Caterpillar = { ItemOdd = 25 }, ["Giant Ant"] = { ItemOdd = 30 }, Snail = { ItemOdd = 40 } } }
+	},
+	["Night Egg"] = {
+		RarityData = { Items = { Raccoon = { ItemOdd = 0.1 }, ["Night Owl"] = { ItemOdd = 3 }, ["Echo Frog"] = { ItemOdd = 7 }, Frog = { ItemOdd = 15 }, Mole = { ItemOdd = 20 }, Hedgehog = { ItemOdd = 40 } } }
+	},
+	["Bee Egg"] = {
+		RarityData = { Items = { ["Queen Bee"] = { ItemOdd = 1 }, ["Petal Bee"] = { ItemOdd = 4 }, ["Bear Bee"] = { ItemOdd = 5 }, ["Honey Bee"] = { ItemOdd = 25 }, Bee = { ItemOdd = 65 } } }
+	},
+	["Anti Bee Egg"] = {
+		RarityData = { Items = { ["Disco Bee"] = { ItemOdd = 0.25 }, Butterfly = { ItemOdd = 1 }, Moth = { ItemOdd = 13.75 }, ["Tarantula Hawk"] = { ItemOdd = 30 }, Wasp = { ItemOdd = 55 } } }
+	},
+	["Premium Anti Bee Egg"] = {
+		RarityData = { Items = { ["Disco Bee"] = { ItemOdd = 0.25 }, Butterfly = { ItemOdd = 1 }, Moth = { ItemOdd = 13.75 }, ["Tarantula Hawk"] = { ItemOdd = 30 }, Wasp = { ItemOdd = 55 } } }
+	},
+	["Premium Night Egg"] = {
+		RarityData = { Items = { Raccoon = { ItemOdd = 1 }, ["Night Owl"] = { ItemOdd = 3 }, ["Echo Frog"] = { ItemOdd = 7 }, Frog = { ItemOdd = 10 }, Mole = { ItemOdd = 16 }, Hedgehog = { ItemOdd = 35 } } }
+	},
+	["Common Summer Egg"] = {
+		RarityData = { Items = { Starfish = { ItemOdd = 50 }, Crab = { ItemOdd = 25 }, Seagull = { ItemOdd = 25 } } }
+	},
+	["Rare Summer Egg"] = {
+		RarityData = { Items = { Flamingo = { ItemOdd = 30 }, Toucan = { ItemOdd = 25 }, ["Sea Turtle"] = { ItemOdd = 20 }, Orangutan = { ItemOdd = 15 }, Seal = { ItemOdd = 10 } } }
+	},
+	["Paradise Egg"] = {
+		RarityData = { Items = { Ostrich = { ItemOdd = 40 }, Peacock = { ItemOdd = 30 }, Capybara = { ItemOdd = 21 }, ["Scarlet Macaw"] = { ItemOdd = 8 }, ["Mimic Octopus"] = { ItemOdd = 1 } } }
+	},
+	["Oasis Egg"] = {
+		RarityData = { Items = { Meerkat = { ItemOdd = 45 }, ["Sand Snake"] = { ItemOdd = 34.5 }, Axolotl = { ItemOdd = 15 }, ["Hyacinth Macaw"] = { ItemOdd = 5 }, ["Fennec Fox"] = { ItemOdd = 0.5 } } }
+	},
+	["Premium Oasis Egg"] = {
+		RarityData = { Items = { Meerkat = { ItemOdd = 45 }, ["Sand Snake"] = { ItemOdd = 34.5 }, Axolotl = { ItemOdd = 15 }, ["Hyacinth Macaw"] = { ItemOdd = 5 }, ["Fennec Fox"] = { ItemOdd = 0.5 } } }
+	},
+	["Dinosaur Egg"] = {
+		RarityData = { Items = { Raptor = { ItemOdd = 35 }, Triceratops = { ItemOdd = 32.5 }, Stegosaurus = { ItemOdd = 28 }, Pterodactyl = { ItemOdd = 3 }, Brontosaurus = { ItemOdd = 1 }, ["T-Rex"] = { ItemOdd = 0.5 } } }
+	},
+	["Primal Egg"] = {
+		RarityData = { Items = { Parasaurolophus = { ItemOdd = 35 }, Iguanodon = { ItemOdd = 32.5 }, Pachycephalosaurus = { ItemOdd = 28 }, Dilophosaurus = { ItemOdd = 3 }, Ankylosaurus = { ItemOdd = 1 }, Spinosaurus = { ItemOdd = 0.5 } } }
+	},
+	["Premium Primal Egg"] = {
+		RarityData = { Items = { Parasaurolophus = { ItemOdd = 34 }, Iguanodon = { ItemOdd = 32.5 }, Pachycephalosaurus = { ItemOdd = 28 }, Dilophosaurus = { ItemOdd = 3 }, Ankylosaurus = { ItemOdd = 1 }, Spinosaurus = { ItemOdd = 0.5 }, ["Egg/Rainbow Premium Primal Egg"] = { ItemOdd = 1 } } }
+	},
+	["Rainbow Premium Primal Egg"] = {
+		RarityData = { Items = { ["Rainbow Parasaurolophus"] = { ItemOdd = 30 }, ["Rainbow Iguanodon"] = { ItemOdd = 25 }, ["Rainbow Pachycephalosaurus"] = { ItemOdd = 20 }, ["Rainbow Dilophosaurus"] = { ItemOdd = 10 }, ["Rainbow Ankylosaurus"] = { ItemOdd = 8 }, ["Rainbow Spinosaurus"] = { ItemOdd = 7 } } }
+	},
+	["Fake Egg"] = {
+		RarityData = { Items = { ["Silver Monkey"] = { ItemOdd = 1 }, Kiwi = { ItemOdd = 1 }, Cow = { ItemOdd = 1 }, ["Sea Otter"] = { ItemOdd = 1 }, Turtle = { ItemOdd = 1 }, ["Polar Bear"] = { ItemOdd = 1 }, ["Orange Tabby"] = { ItemOdd = 1 }, ["Moon Cat"] = { ItemOdd = 1 }, Monkey = { ItemOdd = 1 }, ["Spotted Deer"] = { ItemOdd = 1 }, Rooster = { ItemOdd = 1 }, Pig = { ItemOdd = 1 }, ["Black Bunny"] = { ItemOdd = 1 }, Cat = { ItemOdd = 1 }, Deer = { ItemOdd = 1 }, Chicken = { ItemOdd = 1 }, Dog = { ItemOdd = 1 }, ["Golden Lab"] = { ItemOdd = 1 }, Bunny = { ItemOdd = 1 }, Dragonfly = { ItemOdd = 1 }, ["Praying Mantis"] = { ItemOdd = 1 }, Caterpillar = { ItemOdd = 1 }, ["Giant Ant"] = { ItemOdd = 1 }, Snail = { ItemOdd = 1 }, Raccoon = { ItemOdd = 1 }, Owl = { ItemOdd = 1 }, ["Night Owl"] = { ItemOdd = 1 }, ["Echo Frog"] = { ItemOdd = 1 }, Frog = { ItemOdd = 1 }, Mole = { ItemOdd = 1 }, Hedgehog = { ItemOdd = 1 }, ["Blood Hedgehog"] = { ItemOdd = 1 }, ["Blood Kiwi"] = { ItemOdd = 1 }, ["Blood Owl"] = { ItemOdd = 1 }, ["Chicken Zombie"] = { ItemOdd = 1 }, ["Grey Mouse"] = { ItemOdd = 1 }, Squirrel = { ItemOdd = 1 }, ["Brown Mouse"] = { ItemOdd = 1 }, ["Red Giant Ant"] = { ItemOdd = 1 }, ["Red Fox"] = { ItemOdd = 1 }, ["Queen Bee"] = { ItemOdd = 1 }, ["Bear Bee"] = { ItemOdd = 1 }, ["Petal Bee"] = { ItemOdd = 1 }, Bee = { ItemOdd = 1 }, ["Disco Bee"] = { ItemOdd = 1 }, Butterfly = { ItemOdd = 1 }, ["Tarantula Hawk"] = { ItemOdd = 1 }, Moth = { ItemOdd = 1 }, Wasp = { ItemOdd = 1 }, ["Cooked Owl"] = { ItemOdd = 1 }, ["Pack Bee"] = { ItemOdd = 1 }, Starfish = { ItemOdd = 1 }, Crab = { ItemOdd = 1 }, Seagull = { ItemOdd = 1 }, Flamingo = { ItemOdd = 1 }, Toucan = { ItemOdd = 1 }, ["Sea Turtle"] = { ItemOdd = 1 }, Orangutan = { ItemOdd = 1 }, Seal = { ItemOdd = 1 }, Ostrich = { ItemOdd = 1 }, Peacock = { ItemOdd = 1 }, Capybara = { ItemOdd = 1 }, ["Scarlet Macaw"] = { ItemOdd = 1 }, ["Mimic Octopus"] = { ItemOdd = 1 }, Meerkat = { ItemOdd = 1 }, ["Sand Snake"] = { ItemOdd = 1 }, Axolotl = { ItemOdd = 1 }, ["Hyacinth Macaw"] = { ItemOdd = 1 }, ["Fennec Fox"] = { ItemOdd = 1 }, Hamster = { ItemOdd = 1 }, Raptor = { ItemOdd = 1 }, Triceratops = { ItemOdd = 1 }, Stegosaurus = { ItemOdd = 1 }, Pterodactyl = { ItemOdd = 1 }, Brontosaurus = { ItemOdd = 1 }, ["T-Rex"] = { ItemOdd = 1 }, Iguanodon = { ItemOdd = 1 }, ["Parasaurolophus "] = { ItemOdd = 1 }, Pachycephalosaurus = { ItemOdd = 1 }, Ankylosaurus = { ItemOdd = 1 }, Dilophosaurus = { ItemOdd = 1 }, Spinosaurus = { ItemOdd = 1 }, ["Rainbow Parasaurolophus"] = { ItemOdd = 1 }, ["Rainbow Iguanodon"] = { ItemOdd = 1 }, ["Rainbow Pachycephalosaurus"] = { ItemOdd = 1 }, ["Rainbow Dilophosaurus"] = { ItemOdd = 1 }, ["Rainbow Ankylosaurus"] = { ItemOdd = 1 }, ["Rainbow Spinosaurus"] = { ItemOdd = 1 }, ["Golden Bee"] = { ItemOdd = 1 }, ["Red Dragon"] = { ItemOdd = 1 }, Firefly = { ItemOdd = 1 } } }
+	},
+}
